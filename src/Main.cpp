3fa5a9a3270df6d883747ea055e517a04b2ea8c1
MicheleDusi <m.dusi007@studenti.unibs.it>
2021-02-24 23:34:42 +0100

    New <main> for better presentation

--- src/Main.cpp
+++ src/Main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char **argv) {
 	DEBUG_MARK_PHASE( "Macro Debugger - Main" ) {
 
         DEBUG_LOG( "Hello world!" );
+        DEBUG_LOG_INFO( "Hey, I just greeted you!" );
 
 		// This should work...
         DEBUG_ASSERT_TRUE( 2 + 2 == 4 );
@@ -27,9 +28,13 @@ int main(int argc, char **argv) {
         DEBUG_ASSERT_TRUE( 2 + 3 == 4 );
         DEBUG_ASSERT_FALSE( 2 + 1 == 3 );
 
-        DEBUG_LOG_FAIL( "Something bad just happened!" );
+        DEBUG_LOG_ERROR( "Something bad just happened!" );
+
+		DEBUG_LOG_WARNING( "This phase will end soon..." );
 
     }
+
+	DEBUG_LOG_FAIL( "...Too soon :(" );
 
 	return 0;
 }

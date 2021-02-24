85145e087bf516cad93f6a921fc731859aa87b74
MicheleDusi <m.dusi007@studenti.unibs.it>
2021-02-24 23:32:51 +0100

    Improved basic macro functions

--- include/Basic.hpp
+++ include/Basic.hpp
@@ -13,15 +13,44 @@ namespace macrodebug {
 
     /**
      * Macro function.
-     * Si espande solamente se è attiva la modalità DEBUG.
-     * Può essere usato per espandere qualunque cosa, non solo statements; è compito del programmatore
-     * verificare che un'eventuale espansione vuota non generi problemi.
+     * This function represents the core of the debug library.
+     * If the macro in the "Debug.hpp" is defined, this function will expand.
+     * Otherwise, the preprocessor will remove all its content from the preprocessed source file.
      */
     #ifdef DEBUG_MODE
         #define IF_DEBUG_ACTIVE( ... ) __VA_ARGS__ 		// EXPAND
     #else
         #define IF_DEBUG_ACTIVE( ... ) 					// DO NOT EXPAND
     #endif
+
+
+    // Utilities
+
+    #define EVAL( x )                   x
+
+    /**
+     * Concatenate two parameters.
+     * Example: CONCAT( 123 , abc ) is expanded to 123abc
+     */
+    #define _CONCAT( x, y )				x ## y
+    #define CONCAT( x, y )				_CONCAT( x, y )
+
+    #define _CONCAT_3( x, y, z )        x ## y ## z
+    #define CONCAT_3( x, y, z )			_CONCAT_3( x, y, z )
+
+    /**
+     * Stringify one parameter.
+     * Example: STRINGIFY( 123 ) is expanded to "123"
+     */
+    #define _STRINGIFY( x ) 			#x
+    #define STRINGIFY( x )				_STRINGIFY( x )
+
+    /**
+     * Generate unique IDs for C++ code variables.
+     * All the IDs will start with the same prefix.
+     */
+    #define ID_PREFIX 					debug_var_
+    #define UNIQUE_ID					CONCAT( ID_PREFIX, __LINE__ )
 
 }
 
